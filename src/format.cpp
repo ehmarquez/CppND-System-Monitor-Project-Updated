ba231292eb09ea860430a11cf643479c96185872
Alvin Marquez <alvinmarquez7@gmail.com>
2020-06-21 01:27:46 -0400

    Fix uptime format to be zero-padded when dislayed

--- src/format.cpp
+++ src/format.cpp
@@ -7,7 +7,18 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 string Format::ElapsedTime(long seconds) {
-  return std::to_string(seconds / 3600) + ":" +
-         std::to_string(seconds % 3600 / 60) + ":" +
-         std::to_string(seconds % 60);
+  string hour = std::to_string(seconds / 3600);
+  string minute = std::to_string(seconds % 3600 / 60);
+  string second = std::to_string(seconds % 60);
+
+  if (hour.length() < 2) {
+    hour = "0" + hour;
+  }
+  if (minute.length() < 2) {
+    minute = "0" + minute;
+  }
+  if (second.length() < 2) {
+    second = "0" + second;
+  }
+  return hour + ":" + minute + ":" + second;
 }
